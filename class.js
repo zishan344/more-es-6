725fd4dbadc5657d991ab66805fd82545fc08e1d
zishan344 <zishanahmed344@gmail.com>
2022-02-20 15:17:30 +0600

    done the class.js file

---
+++ class.js
@@ -0,0 +1,21 @@
+class Support {
+  name;
+  constructor(name, home, town) {
+    this.name = name;
+    this.home = home;
+    this.town = town;
+  }
+  home;
+  town;
+
+  startSessions() {
+    console.log(this.name + " start the support session");
+  }
+}
+
+const marouf = new Support("marouf", "Bangladesh", "ctg");
+const aamir = new Support("amir khan", "india", "mombai");
+// aamir.startSessions();
+// marouf.startSessions();
+console.log(marouf);
+console.log(aamir);
